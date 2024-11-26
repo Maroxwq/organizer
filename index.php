4506af91480aacd47e94c73817154004ed18989d
Sasha <zwqsashafurtuna@gmail.com>
2024-11-26 22:44:47 +0000

    changes

--- index.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-echo 'Hello World<br>';
-echo 'Final test push';
-
-?>
---
+++ public/thing.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>ORGANIZER</h1>
+    <ul>
+        <a href="about.php"><li>123</li></a>
+        <a href="index.php"><li>456</li></a>
+    </ul>
+</body>
+</html>
