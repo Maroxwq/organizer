f265d74c65e7c0fcdc8e64c6af2ca8fa3d0f9708
Sasha <zwqsashafurtuna@gmail.com>
2024-12-04 19:02:22 +0000

    rendertemplate

--- public/header.php
+++ public/header.php
@@ -1,21 +1,17 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title></title>
+<body>
     <link rel="stylesheet" href="css/header.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet">
-</head>
-<body>
+
     <?php
-        if ($_SERVER['REQUEST_URI'] == '/index.php') {
+        if ($_SERVER['PHP_SELF'] == '/index.php') {
             $mainun = true;
-        } elseif ($_SERVER['REQUEST_URI'] == '/notes.php'){
+        } elseif ($_SERVER['REQUEST_URI'] == '/Main'){
+            $mainun = true;
+        } elseif ($_SERVER['REQUEST_URI'] == '/Notes'){
             $noteun = true;;
-        } elseif ($_SERVER['REQUEST_URI'] == '/about.php'){
+        } elseif ($_SERVER['REQUEST_URI'] == '/About'){
             $aboutun = true;;
         }
     ?>
@@ -26,11 +22,10 @@
         </div>
         
         <div class="section">
-            <div class="hbutton <?= $mainun ? 'mainun' : '' ?>"><a href="index.php">Main</a></div>
-            <div class="hbutton <?= $noteun ? 'notesun' : '' ?>"><a href="notes.php">Notes</a></div>
-            <div class="hbutton <?= $aboutun ? 'aboutun' : '' ?>"><a href="about.php">About</a></div>
+            <div class="hbutton <?= $mainun ? 'mainun' : '' ?>"><a href="Main">Main</a></div>
+            <div class="hbutton <?= $noteun ? 'notesun' : '' ?>"><a href="Notes">Notes</a></div>
+            <div class="hbutton <?= $aboutun ? 'aboutun' : '' ?>"><a href="About">About</a></div>
             <button class="hbutton"><a href="login.php" class="login">Login</a></button>
         </div>
     </div>
 </body>
-</html>
--- public/index.php
+++ public/index.php
@@ -1,32 +1,24 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Main page</title>
-</head>
-<body>
-    <?php require('header.php')?>
+<?php declare(strict_types=1);
+        function render(string $templatePath, array $params): string
+        {
+            extract($params);
+            ob_start();
+            ob_implicit_flush(false);
 
-    <div class="content1">
-        <div class="info">
-            <h1>Main page</h1>
-            <p>Subheading that sets up context, shares more info about the website, or generally gets people psyched to keep scrolling.</p>
-            <button>Button</button>
-        </div>
-        <h2>Section heading</h2>
-        <div class="subheading">
-            <p class="titl">Subheading</p>
-            <p class="txt">Body text for whatever you’d like to expand on the main point.</p>
-            <p class="titl">Subheading</p>
-            <p class="txt">Body text for whatever you’d like to say. Add main takeaway points, quotes, anecdotes.</p>
-            <p class="titl">Subheading</p>
-            <p class="txt">Body text for whatever you’d like to add more to the main point. It provides details, explanations, and context.</p>
-        </div>
-        <div class="btns">
-            <button class="btn1">Button</button>
-            <button class="btn2">Secondary button</button>
-        </div>
-    </div>
-</body>
-</html>
+            require $templatePath;
+
+            return ob_get_clean();
+        }
+
+        $renderAbout = render('about.php', ['title' => 'About']);
+        $renderMain = render('main.php', ['title' => 'Main']);
+        $renderNotes = render('notes.php', ['title' => 'Notes']);
+
+        if ($_SERVER['REQUEST_URI'] == '/About') {
+            echo $renderAbout;
+        } elseif ($_SERVER['REQUEST_URI'] == '/Notes') {
+            echo $renderNotes;
+        } else {
+            echo $renderMain;
+        }
+    ?>
