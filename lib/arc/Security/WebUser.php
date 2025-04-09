6294372d7837fffc7758de77256b68c34b7d52ec
Sasha <zwqsashafurtuna@gmail.com>
2025-04-09 07:53:49 +0000

    Deleting draft; ORG-33, 34, 35

--- lib/arc/Security/WebUser.php
+++ lib/arc/Security/WebUser.php
@@ -7,27 +7,31 @@ use Arc\Http\Session;
 use Arc\Security\IdentityInterface;
 
 class WebUser {
-    private ?IdentityInterface $identity = null;
-
     public function __construct(private Session $session, private Repository $userRepository) {}
 
-    public function isAuthenticated(): bool {
+    public function isAuthenticated(): bool
+    {
         return $this->session->has('user_id');
     }
 
-    public function getIdentity(): ?IdentityInterface {
+    public function getIdentity(): ?IdentityInterface
+    {
         if ($this->isAuthenticated()) {
             return $this->userRepository->findOne($this->session->get('user_id'));
         }
 
         return null;
     }
-    public function login(IdentityInterface $identity): bool {
+
+    public function login(IdentityInterface $identity): bool
+    {
         $this->session->set('user_id', $identity->getUserId());
 
         return true;
     }
-    public function logout(): bool {
+
+    public function logout(): bool
+    {
         $this->session->delete('user_id');
 
         return true;
